9073631d6ec37fd8ca319c1758642857e49e481c
likeshan168 <likeshan168@163.com>
2017-08-01 15:59:08 +0800

    添加项目文件。

---
+++ Nop.Plugin.Widgets.HelloWorld/Controllers/WidgetsHelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Nop.Plugin.Widgets.HelloWorld.Models;
+using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Nop.Plugin.Widgets.HelloWorld.Controllers
+{
+    public class WidgetsHelloWorldController: BasePluginController
+    {
+        public WidgetsHelloWorldController()
+        {
+
+        }
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            return View("~/Plugins/Widgets.HelloWorld/Views/WidgetsHelloWorld/Configure.cshtml");
+        }
+        [AdminAuthorize]
+        [HttpPost]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            return Configure();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PublicInfo(string widgetZone, object additionalData = null)
+        {
+            return View("~/Plugins/Widgets.HelloWorld/Views/WidgetsHelloWorld/PublicInfo.cshtml", null);
+        }
+    }
+}
